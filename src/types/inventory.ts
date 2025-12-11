6eb33c616fcab2b0ed10d7ccbdf78d61e4f98b8d
Jamshaa <125591505+Jamshaa@users.noreply.github.com>
2025-12-12 00:46:34 +0100

    Delete src directory

--- src/types/inventory.ts
+++
@@ -1,9 +0,0 @@
-import { Resource } from "halfred";
-
-export interface Inventory extends Resource {
-    name: string;
-    description?: string;
-    location: string;
-    totalStock: number;
-
-}
